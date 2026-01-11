c4e6ea174939876353bc8a8de99189d7af317a07
ayarika <ayarikaworks16@gmail.com>
2026-01-11 19:04:05 +0700

    Fix countdown badge

--- resources/views/organizerprofile.blade.php
+++ resources/views/organizerprofile.blade.php
@@ -339,7 +339,7 @@
                         @forelse($organizer->events as $event)
                             <div class="bootcamp-card clickable-card"
                                 onclick="window.location='{{ route('bootcamp.detail', $event->id) }}'"
-                                data-start-date="{{ \Carbon\Carbon::parse($event->start_date)->format('Y-m-d\TH:i:s') }}">
+                                data-start-date="{{ \Carbon\Carbon::parse($event->start_date)->toIso8601String() }}">
                                 
                                 <div class="bootcamp-card-banner">
                                     <img src="{{ $event->image_path && file_exists(public_path($event->image_path))
